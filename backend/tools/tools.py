b3e150b4bb9c44d86ba7c4b020160fccac7624de
Ishfaq-code <ishfaqmcb12@gmail.com>
2026-03-28 23:06:15 -0400

    feat: frontend restyling

--- backend/tools/tools.py
+++
@@ -1,9 +0,0 @@
-from search_user_messages import search_user_messsages
-
-tools = [
-    {
-        "name": "search_user_messages",
-        "description": "Search stored user messages",
-        "func": search_user_messsages,
-    }
-]
